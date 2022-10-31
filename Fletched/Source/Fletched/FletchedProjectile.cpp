1fcdefa53ffc99754a2fffdc928d461b1bd08413
raymondbecking <raymondbecking@gmail.com>
2022-10-31 16:59:07 +0100

    Refactor of Projectile classes

--- Fletched/Source/Fletched/FletchedProjectile.cpp
+++ Fletched/Source/Fletched/FletchedProjectile.cpp
@@ -37,44 +37,47 @@ void AFletchedProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor
                                 FVector NormalImpulse, const FHitResult& Hit)
 {	
 	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr))
+	if (OtherActor == nullptr || OtherActor == this || OtherComp == nullptr)
 	{
-		//Behavior for Physics objects being hit
-		if(OtherComp->IsSimulatingPhysics())
-		{
-			OtherComp->AddImpulseAtLocation(GetVelocity() * .3f, GetActorLocation());
-			//Attach this arrow to a physics actor
-			this->AttachToActor(OtherActor, FAttachmentTransformRules::KeepWorldTransform);
-		}
-
-		//Behaviour for Characters being hit
-		if(OtherComp->IsA(USkeletalMeshComponent::StaticClass()))
-		{
-			//Basic Dismemberment TODO: Move to Fletched Character/Enemy and cut up the skeletalmesh in different pieces
-			/*USkeletalMeshComponent* HitMesh = Cast<USkeletalMeshComponent>(OtherComp);
-			HitMesh->BreakConstraint(NormalImpulse, Hit.Location, Hit.BoneName);*/
-
-			//Attach projectile to a character
-			ACharacter* HitCharacter = Cast<ACharacter>(OtherActor);
-			if(HitCharacter != nullptr)
-			{			
-				this->AttachToComponent(HitCharacter->GetMesh(), FAttachmentTransformRules::KeepWorldTransform, Hit.BoneName);				
-			}
-
-			//Damage the actor that was hit by this projectile
-			AActor* HitActor = Hit.GetActor();
-			if(HitActor == nullptr || GetInstigator() == nullptr)
-			{
-				return;				
-			}
-			const FPointDamageEvent DamageEvent(Damage, Hit, FVector::One(), nullptr);
-			if(AController* OwnerController = this->GetInstigator()->GetController(); OwnerController != nullptr)
-			{
-				HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);						
-			}			
-		}
-
-		//Disable arrow collision after hitting something
-		this->SetActorEnableCollision(false);
+		return;
 	}
+	//Behavior for Physics objects being hit
+	if (OtherComp->IsSimulatingPhysics())
+	{
+		OtherComp->AddImpulseAtLocation(GetVelocity() * .3f, GetActorLocation());
+		//Attach this arrow to a physics actor
+		this->AttachToActor(OtherActor, FAttachmentTransformRules::KeepWorldTransform);
+	}
+
+	//Behaviour for Characters being hit
+	if (OtherComp->IsA(USkeletalMeshComponent::StaticClass()))
+	{
+		//Basic Dismemberment TODO: Move to Fletched Character/Enemy and cut up the skeletalmesh in different pieces
+		/*USkeletalMeshComponent* HitMesh = Cast<USkeletalMeshComponent>(OtherComp);
+		HitMesh->BreakConstraint(NormalImpulse, Hit.Location, Hit.BoneName);*/
+
+		//Attach projectile to a character
+		ACharacter* HitCharacter = Cast<ACharacter>(OtherActor);
+		if (HitCharacter != nullptr)
+		{
+			this->AttachToComponent(HitCharacter->GetMesh(), FAttachmentTransformRules::KeepWorldTransform,
+			                        Hit.BoneName);
+		}
+
+		//Damage the actor that was hit by this projectile
+		AActor* HitActor = Hit.GetActor();
+		if (HitActor == nullptr || GetInstigator() == nullptr)
+		{
+			return;
+		}
+		const FPointDamageEvent DamageEvent(Damage, Hit, FVector::One(), nullptr);
+		if (AController* OwnerController = this->GetInstigator()->GetController(); OwnerController != nullptr)
+		{
+			HitActor->TakeDamage(Damage, DamageEvent, OwnerController, this);
+		}
+	}
+
+	//Disable arrow collision after hitting something
+	this->SetActorEnableCollision(false);
+	
 }
